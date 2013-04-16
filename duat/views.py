52877901becc746cb3f559d4f03631e626c32cbb
Chris Bailey <c.bailey@bristol.ac.uk>
2013-04-16 11:25:39 +0100

    Replacing tabs with spaces

--- duat/views.py
+++ duat/views.py
@@ -19,122 +19,122 @@ from duat.models import Feedback, Project
 logger = logging.getLogger(__name__)
 
 def post(request, project_name):
-	"""
-	Store a posted feedback submission
-	"""
-	project = Project.objects.get(name=project_name)
+    """
+    Store a posted feedback submission
+    """
+    project = Project.objects.get(name=project_name)
 
-	comment = page = referrer = None
-	data = request.POST.get('data',None)
+    comment = page = referrer = None
+    data = request.POST.get('data',None)
 
-	if data:
-		j = json.loads(data.encode("utf-8"))
-		html = j['html']
-		comment = j['comment']
-		referrer = j['referrer']
-		page = j['url']
-		
-		# process html contents...
-		
-		# remove all scripts
-		# NB - We are not attempting to comprehensively block XSS, 
-		# rather simply disable any javascript from loading
-		html = re.sub('<script(.*?)<\/script>','',html, 
-					  flags=(re.IGNORECASE | re.DOTALL))
-		
-		pos = html.find('<head')
-		pos = html.find('>',pos)+1
-		
-		# insert base tag
-		if '<base' not in html and page:
-			host = page[:page.find('/',10)]
-			html = html[0:pos] + "<base href='"+host+"'></base>" + html[pos:]
+    if data:
+        j = json.loads(data.encode("utf-8"))
+        html = j['html']
+        comment = j['comment']
+        referrer = j['referrer']
+        page = j['url']
+        
+        # process html contents...
+        
+        # remove all scripts
+        # NB - We are not attempting to comprehensively block XSS, 
+        # rather simply disable any javascript from loading
+        html = re.sub('<script(.*?)<\/script>','',html, 
+                      flags=(re.IGNORECASE | re.DOTALL))
+        
+        pos = html.find('<head')
+        pos = html.find('>',pos)+1
+        
+        # insert base tag
+        if '<base' not in html and page:
+            host = page[:page.find('/',10)]
+            html = html[0:pos] + "<base href='"+host+"'></base>" + html[pos:]
 
-	if comment and html:
-		feedback = Feedback(project = project,
-							comment = comment,
-							html = html,
-							user_agent = request.META.get('HTTP_USER_AGENT',''),
-							referrer = referrer,
-							page = page)
-		feedback.save()
-	
-		logger.info('Saved issue %s' % feedback.id)
-		
-		generate_screenshot(feedback)
-		
-		if project.notify_admin and project.admin.email:
-			try:
-				url = reverse('admin:%s_%s_change' % (feedback._meta.app_label,
-													  feedback._meta.module_name),
-							  args=[feedback.id])
-				if request.is_secure():
-					url = 'https://' + request.META.get('HTTP_HOST') + url
-				else:
-					url = 'http://' + request.META.get('HTTP_HOST') + url
-				send_mail('DUAT: feedback recieved',
-						  'You can view this item here:\n%s' % url, 
-						  'webmaster@%s ' % settings.HOSTNAME, 
-						  [project.admin.email],
-						  fail_silently=False)
-			except:
-				logger.warn("Unable to notify admin")
+    if comment and html:
+        feedback = Feedback(project = project,
+                            comment = comment,
+                            html = html,
+                            user_agent = request.META.get('HTTP_USER_AGENT',''),
+                            referrer = referrer,
+                            page = page)
+        feedback.save()
+    
+        logger.info('Saved issue %s' % feedback.id)
+        
+        generate_screenshot(feedback)
+        
+        if project.notify_admin and project.admin.email:
+            try:
+                url = reverse('admin:%s_%s_change' % (feedback._meta.app_label,
+                                                      feedback._meta.module_name),
+                              args=[feedback.id])
+                if request.is_secure():
+                    url = 'https://' + request.META.get('HTTP_HOST') + url
+                else:
+                    url = 'http://' + request.META.get('HTTP_HOST') + url
+                send_mail('DUAT: feedback recieved',
+                          'You can view this item here:\n%s' % url, 
+                          'webmaster@%s ' % settings.HOSTNAME, 
+                          [project.admin.email],
+                          fail_silently=False)
+            except:
+                logger.warn("Unable to notify admin")
 
-		return HttpResponse("Thanks")
-	else:
-		logger.error('Not saved - missing data')
-		return HttpResponseServerError('Something went wrong')
-	
-	
+        return HttpResponse("Thanks")
+    else:
+        logger.error('Not saved - missing data')
+        return HttpResponseServerError('Something went wrong')
+    
+    
 def view(request, project_name, id):
-	project = Project.objects.get(name=project_name)
-	feedback = Feedback.objects.get(pk=id, project=project)
-	
-	html = feedback.html
-	pos = html.find('<head')
-	pos = html.find('>',pos)+1
-	
-	# add our own admin script
-	url = request.META.get('HTTP_HOST') + reverse('admin')
-	html = html[0:pos] + "<script src='//%s'></script>" % url + html[pos:]
+    project = Project.objects.get(name=project_name)
+    feedback = Feedback.objects.get(pk=id, project=project)
+    
+    html = feedback.html
+    pos = html.find('<head')
+    pos = html.find('>',pos)+1
+    
+    # add our own admin script
+    url = request.META.get('HTTP_HOST') + reverse('admin')
+    html = html[0:pos] + "<script src='//%s'></script>" % url + html[pos:]
 
-	return HttpResponse(html)
-	
+    return HttpResponse(html)
+    
 def generate_screenshot(feedback):
-	""" Pass the html onto PhantomJS to convert into an image """
-	PHANTOMJS_EXECUTABLE = getattr(settings, 'PHANTOMJS_EXECUTABLE', 
-								   'phantomjs')
-	output_dir = os.path.join(settings.STATIC_ROOT, 'screenshots')
-	output_file = "%s/%s.jpg" % (output_dir, feedback.id) 
+    """ Pass the html onto PhantomJS to convert into an image """
+    PHANTOMJS_EXECUTABLE = getattr(settings, 'PHANTOMJS_EXECUTABLE', 
+                                   'phantomjs')
+    output_dir = os.path.join(settings.STATIC_ROOT, 'screenshots')
+    output_file = "%s/%s.jpg" % (output_dir, feedback.id) 
 
-	# create a temp file to write our HTML to
-	input_file = tempfile.NamedTemporaryFile(delete=True)
+    # create a temp file to write our HTML to
+    input_file = tempfile.NamedTemporaryFile(delete=True)
 
-	input_file.write(smart_str(feedback.html))
-	# insert base so that static resources are loaded correctly
+    input_file.write(smart_str(feedback.html))
+    # insert base so that static resources are loaded correctly
 
-	# construct parameters to our phantom instance
-	args = [PHANTOMJS_EXECUTABLE,
-			os.path.dirname(__file__)+"/renderhtml.js",
-			input_file.name,
-			output_file]
-	
-	# create the process
-	p = call(args)
-	
+    # construct parameters to our phantom instance
+    args = [PHANTOMJS_EXECUTABLE,
+            os.path.dirname(__file__)+"/renderhtml.js",
+            input_file.name,
+            output_file]
+    
+    # create the process
+    p = call(args)
+    
 def generate_js(request, filename, project_name=None):
-	""" 
-	Returns a javascript file generated using django's templating mechanism
-	"""
-	project = path = None
-	if project_name:
-		project = Project.objects.get(name=project_name)
-		path = reverse('post', kwargs={'project_name':project.name})
-	context = RequestContext(request)
-	host = request.META.get('HTTP_HOST')
-	return HttpResponse(render_to_response(filename,
-										   {'project':project,
-											'host' : host,
-											'path': path},
-										   context_instance=context),
-						mimetype = 'application/javascript')
+    """ 
+    Returns a javascript file generated using django's templating mechanism
+    """
+    project = path = None
+    if project_name:
+        project = Project.objects.get(name=project_name)
+        path = reverse('post', kwargs={'project_name':project.name})
+    context = RequestContext(request)
+    host = request.META.get('HTTP_HOST')
+    return HttpResponse(render_to_response(filename,
+                                           {'project':project,
+                                            'host' : host,
+                                            'path': path},
+                                           context_instance=context),
+                        mimetype = 'application/javascript')
