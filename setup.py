61ac8442c3eadf3659b499a567a8754934952c8e
Chris Bailey <c.bailey@bristol.ac.uk>
2014-05-13 12:53:53 +0100

    updating package number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-duat',
-    version='0.4',
+    version='0.5',
     packages=['duat'],
     include_package_data=True,
     license='MIT License',
