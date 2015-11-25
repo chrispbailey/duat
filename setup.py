37db7284e48cd79607e59f811e5f0db20b1e5b2a
Chris Bailey <chris.p.bailey@gmail.com>
2015-11-25 11:13:33 +0000

    Adding migrations folder to distribution package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-duat',
-    version='0.9',
+    version='1.0',
     packages=['duat'],
     include_package_data=True,
     license='MIT License',
